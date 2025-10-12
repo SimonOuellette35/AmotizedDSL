e7b5496aa91446317b7fefea5069a7b5919f39c0
SimonOuellette35 <chiefscientist@outlook.com>
2025-10-11 23:37:46 -0400

    updates to the virtual env

--- VirtualEnv/batched_env.py
+++ VirtualEnv/batched_env.py
@@ -8,27 +8,37 @@ import numpy as np
 
 class BatchedAmotizedDSLEnv:
 
-    def __init__(self, input_batch, output_batch, obj_masks_batch):
+    def __init__(self, input_batch, output_batch, obj_masks_batch, target_comments):
         # input_batch, output_batch are:
         # [batch_size, K, flattened tokenized grid (MAX 931)]
         self.input_batch = input_batch
         self.input_batch_DSL = []
         self.obj_masks_batch = obj_masks_batch
         self.output_batch = output_batch
+        self.target_comments = target_comments
+
         self.batch_size = len(input_batch)
         self.k = len(input_batch[0])
 
         self.batched_prog_state = None
         self.batched_targets = None
+        self.batched_comments = None
 
     def init(self):
         batch_init_state = []
         targets = []
 
+        current_comments = []
         for batch_idx in range(self.batch_size):
             tmp_states = []        
             tmp_targets = []
 
+            batch_comments = []
+            batch_comments.append("Grid")  # the input grid, which is always there
+            #batch_comments.append(target_comments[batch_idx][0])
+
+            current_comments.append(batch_comments)
+        
             for k in range(self.k):
 
                 detok_outp_grid = tok.detokenize_grid_unpadded(self.output_batch[batch_idx][k])
@@ -50,6 +60,7 @@ class BatchedAmotizedDSLEnv:
 
         self.batched_prog_state = batch_init_state
         self.batched_targets = targets    
+        self.batched_comments = current_comments
         return batch_init_state, targets
 
     def is_neural_primitive(self, instr_step):
@@ -69,8 +80,8 @@ class BatchedAmotizedDSLEnv:
         else:
             return False, prim_name
 
-    def act(self, batch_action_sequences):
-        
+    def act(self, batch_action_sequences, batch_comments):
+
         # Execute the instruction sequence to get the next state
         tmp_batch_output = []
         for batch_idx in range(len(self.current_prog_state)):
@@ -102,19 +113,35 @@ class BatchedAmotizedDSLEnv:
                 tmp_batch_output.append(tmp_output)
 
         for batch_idx in range(len(tmp_batch_output)):
+            if not isinstance(tmp_batch_output[batch_idx][0], pi.DeleteAction):
+                if batch_comments is not None:
+                    #print(f"Adding comment: {target_comments[batch_idx][prog_step_idx]}")
+                    self.batched_comments[batch_idx].append(batch_comments[batch_idx])
+                else:
+                    self.batched_comments[batch_idx].append('END')
+                    
+        for batch_idx in range(len(tmp_batch_output)):
             if isinstance(tmp_batch_output[batch_idx][0], pi.DeleteAction):
                 kept_states = []
+                kept_comments = []
                 for k in range(len(self.batched_prog_state[batch_idx])):
                     kept_states.append([])
                     for i, state in enumerate(self.batched_prog_state[batch_idx][k]):
                         if i != tmp_batch_output[batch_idx][k].state_idx:
                             kept_states[k].append(state)
 
+                for i in range(len(self.batched_comments[batch_idx])):
+                    if i != tmp_batch_output[batch_idx][k].state_idx:
+                        kept_comments.append(self.batched_comments[batch_idx][i])
+
                 self.batched_prog_state[batch_idx] = kept_states
+                self.batched_comments[batch_idx] = kept_comments
+
+                #print(f"A delete occurred for state_idx {tmp_batch_output[batch_idx][k].state_idx}. New current_comments: {current_comments}")
             elif tmp_batch_output[batch_idx][0] is not None:
                 for k in range(len(self.batched_prog_state[batch_idx])):
                     self.batched_prog_state[batch_idx][k].append(tmp_batch_output[batch_idx][k])
 
         # TODO: calculate reward. Currently unused, though.
         reward_batch = np.zeros([self.batch_size])
-        return self.batched_prog_state, reward_batch
+        return self.batched_prog_state, self.batched_comments #reward_batch
