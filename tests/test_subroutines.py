5b18d45ef3285537c1034fb259518b02ad12e466
SimonOuellette35 <chiefscientist@outlook.com>
2026-03-27 17:13:19 -0400

    fixed subroutines and unit tests

--- tests/test_subroutines.py
+++ tests/test_subroutines.py
@@ -66,12 +66,9 @@ def test_expand_subroutine():
         'set_x(N+0, N+1)',
         'del(N+0)',
         'del(N+0)',
-        # concat_h(N+0, N+1)
+        # concat_h(N+0, N+1)        
         'add(N+1.width, N+1.x)',
-        'set_pixels(N+0, N+2, N+0.y, N+1.c)',
-        'del(N+0)',
-        'del(N+0)',
-        'del(N+0)',
+        'set_pixels(N+0, N+1, N+0.y, N+1.c)',
 
         # last 2 dels
         'del(N+0)',
@@ -81,17 +78,11 @@ def test_expand_subroutine():
     expected_prog3 = [
         # concat_h(N+0, N+0)
         'add(N+0.width, N+0.x)',
-        'set_pixels(N+0, N+2, N+0.y, N+0.c)',
-        'del(N+0)',
-        'del(N+0)',
-        'del(N+0)',
+        'set_pixels(N+0, N+1, N+0.y, N+0.c)',
 
         # concat_v(N+1, N+1)
         'add(N+1.height, N+1.y)',
-        'set_pixels(N+1, N+1.x, N+2, N+1.c)',
-        'del(N+0)',
-        'del(N+0)',
-        'del(N+0)',
+        'set_pixels(N+1, N+1.x, N+3, N+1.c)',
 
         # 2 last dels
         'del(N+0)',
@@ -101,13 +92,13 @@ def test_expand_subroutine():
     expected_prog4 = [
         # flip_h(N+0)
         'sub(N+0.max_x, N+0.x)',
-        'set_pixels(N+0, N+1, N+0.y, N+0.c)',
+        'set_x(N+0, N+1)',
         'del(N+0)',
         'del(N+0)',
 
         # flip_v(N+0)
         'sub(N+0.max_y, N+0.y)',
-        'set_pixels(N+0, N+0.x, N+1, N+0.c)',
+        'set_y(N+0, N+1)',
         'del(N+0)',
         'del(N+0)',
 
@@ -116,17 +107,11 @@ def test_expand_subroutine():
 
         # concat_h(N+0, N+1)
         'add(N+1.width, N+1.x)',
-        'set_pixels(N+0, N+2, N+0.y, N+1.c)',
-        'del(N+0)', 
-        'del(N+0)',
-        'del(N+0)',
+        'set_pixels(N+0, N+1, N+0.y, N+1.c)',
 
         # concat_h(N+1, N+0)
         'add(param2.width, N+0.x)',
         'set_pixels(N+1, N+2, N+1.y, N+0.c)',
-        'del(N+0)', 
-        'del(N+0)',
-        'del(N+0)',
 
         # 2 del stements
         'del(N+0)',
@@ -135,9 +120,6 @@ def test_expand_subroutine():
         # concat_v(N+0, N+1)
         'add(N+1.height, N+1.y)',
         'set_pixels(N+0, N+0.x, N+2, N+1.c)',
-        'del(N+0)',
-        'del(N+0)',
-        'del(N+0)',
 
         # 2 last dels
         'del(N+0)',
